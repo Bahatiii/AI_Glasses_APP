bc9283868a9f3a14ec84c2d41c90c8eab8d459ad
zhj <202464820255@mail.scut.edu.cn>
2025-07-22 18:40:39 +0800

    好学生不翻墙，用镜像

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,17 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        // 阿里云镜像（覆盖 Maven Central、Google、JCenter 等）
+        maven { setUrl("https://maven.aliyun.com/repository/public/") }
+        maven { setUrl("https://maven.aliyun.com/repository/google/") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter/") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin/") }
+        // 华为云镜像
+        maven { setUrl("https://repo.huaweicloud.com/repository/maven/") }
+        // 腾讯云镜像
+        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+        // 网易镜像
+        maven { setUrl("https://mirrors.163.com/maven/repository/maven-public/") }
+
         mavenCentral()
         gradlePluginPortal()
     }
@@ -14,13 +19,25 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
+        // 阿里云镜像（覆盖 Maven Central、Google、JCenter 等）
+        maven { setUrl("https://maven.aliyun.com/repository/public/") }
+        maven { setUrl("https://maven.aliyun.com/repository/google/") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter/") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin/") }
+        // 华为云镜像
+        maven { setUrl("https://repo.huaweicloud.com/repository/maven/") }
+        // 腾讯云镜像
+        maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+        // 网易镜像
+        maven { setUrl("https://mirrors.163.com/maven/repository/maven-public/") }
+
         google()
         mavenCentral()
         flatDir {
             dirs("app/libs")
         }
     }
-
-    rootProject.name = "My Application"
-    include(":app")
 }
+
+rootProject.name = "My Application"
+include(":app")
