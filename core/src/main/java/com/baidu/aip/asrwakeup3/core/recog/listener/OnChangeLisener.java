dad28a17aed6a0650579cd990acda5c0c1700263
Bahatiii <1656570054@qq.com>
2025-07-28 16:56:35 +0800

    换了个集成包

---
+++ core/src/main/java/com/baidu/aip/asrwakeup3/core/recog/listener/OnChangeLisener.java
@@ -0,0 +1,10 @@
+package com.baidu.aip.asrwakeup3.core.recog.listener;
+
+/**
+ * @author : LGQ
+ * @date : 2021/02/26 14
+ * @desc :
+ */
+public interface OnChangeLisener {
+    void onChanged(String date);
+}
